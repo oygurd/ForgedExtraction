71940d168eadd7eaaba7c86898d0c1722dfd292c
oygurd <nirmula3d@gmail.com>
2026-04-12 22:26:11 +0300

    deleted old scripts
    new ones on the way

--- kubejs/server_scripts/create-ak47recipe .js
+++ kubejs/server_scripts/create-ak47recipe .js
@@ -1,27 +1,16 @@
 ServerEvents.recipes(event => {
     event.remove({ id: 'tacz:gun/ak47' });
-    event.recipes.create.sequenced_assembly(
-        // Outputs:
-        [
-            Item.of('tacz:modern_kinetic_gun', '{GunId:"tacz:ak47"}').withChance(100), // Main output, will appear in JEI as the result
-
+    event.custom({
+        type: "tacz:gun_smith_table_crafting",
+        materials: [
+            { item: { item: 'minecraft:iron_ingot' }, count: 5 },
+            { item: { item: 'minecraft:oak_button' }, count: 1 },
         ],
-        // Input:
-        '#forge:rods/iron',
-        // Sequence:
-        [
-            // The transitional item set by `transitionalItem('create:incomplete_large_cogwheel')` is the item used during the intermediate stages of the assembly
-            // Like a normal recipe function, it's used as a sequence step in this array. Input and output have the transitional item
-            event.recipes.create.deploying('unfinished_weapons_parts:unfinished_akm', ['unfinished_weapons_parts:unfinished_akm', 'create:iron_sheet',]),
-            event.recipes.create.deploying('unfinished_weapons_parts:unfinished_akm', ['unfinished_weapons_parts:unfinished_akm', '#minecraft:logs',]),
-            event.recipes.create.deploying('unfinished_weapons_parts:unfinished_akm', ['unfinished_weapons_parts:unfinished_akm', 'minecraft:tripwire_hook',]),
-            event.recipes.create.deploying('unfinished_weapons_parts:unfinished_akm', ['unfinished_weapons_parts:unfinished_akm', 'minecraft:piston',]),
-            event.recipes.create.deploying('unfinished_weapons_parts:unfinished_akm', ['unfinished_weapons_parts:unfinished_akm', 'minecraft:obsidian',]),
-            event.recipes.create.pressing('unfinished_weapons_parts:unfinished_akm', 'unfinished_weapons_parts:unfinished_akm'),
-            event.recipes.create.filling(Item.of('tacz:modern_kinetic_gun', '{GunId:"tacz:ak47"}'), [Fluid.of('createdieselgenerators:plant_oil'), 'unfinished_weapons_parts:unfinished_akm'])
+        result: {
+            type: "gun",
+            id: "tacz:ak47",
+            count: 1
+        },
 
-        ]
-    )
-        .transitionalItem('unfinished_weapons_parts:unfinished_akm') // Set the transitional item
-        .loops(3) // Set the number of loops
+    });
 });
